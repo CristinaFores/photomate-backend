4886757d93e57f2a5cfb48304b61fbc1ae578e66
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-21 20:55:39 +0100

    Add startServer and debugCreator

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "./loadEnviroment.js";
+import debugCreator from "debug";
+import environment from "./loadEnviroment.js";
+import startServer from "./server/startServer.js";
+import app from "./server/app.js";
+import chalk from "chalk";
+
+const debug = debugCreator("post:server");
+
+const { port } = environment;
+
+try {
+  await startServer(app, Number(port));
+  debug(chalk.magenta("Start server"));
+} catch (error: unknown) {
+  debug(
+    chalk.red(`Error connecting to the database ${(error as Error).message}`)
+  );
+}
