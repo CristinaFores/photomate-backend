2bfee5126f62561b88703dd2ab678bad19b0a563
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-21 21:05:54 +0100

    Resololve conflicts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import "./loadEnviroment.js";
 import debugCreator from "debug";
 import environment from "./loadEnviroment.js";
 import startServer from "./server/startServer.js";
-import app from "./server/app.js";
 import chalk from "chalk";
+import app from "./server/app.js";
 
 const debug = debugCreator("post:server");
 
