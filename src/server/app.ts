596137adcc2af35f400ebb34f119b5ecdf544379
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-22 10:42:51 +0100

    Add app.use(unknownEndpoint);

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,7 @@ import "../loadEnviroment.js";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { unknownEndpoint } from "./middelwares/errors.js";
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(unknownEndpoint);
 export default app;
