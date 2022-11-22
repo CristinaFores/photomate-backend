432fd09038579a811011b42ca8deac90957a7737
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-22 11:40:28 +0100

    Add middelware generalError

--- src/server/middelwares/errors.ts
+++ src/server/middelwares/errors.ts
@@ -6,9 +6,13 @@ export const httpStatusCodes = {
   clientErrors: {
     notFoundError: 404,
   },
+  serverError: {
+    errorGeneral: 500,
+  },
 };
 const {
   clientErrors: { notFoundError },
+  serverError: { errorGeneral },
 } = httpStatusCodes;
 
 export const unknownEndpoint = (
@@ -25,4 +29,17 @@ export const unknownEndpoint = (
       "Unknown endpoint"
     )
   );
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line no-unused-vars
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode ?? errorGeneral;
+  const publicMessage = error.publicMessage || "There is a error general";
+
+  res.status(statusCode).json({ error: publicMessage });
 };
