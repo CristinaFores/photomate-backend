66d58a7240dcca26c9a37ccffe0fa398416ffb32
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-24 16:57:41 +0100

    Create loginUser function

--- src/server/controllers/usersControllers.ts
+++ src/server/controllers/usersControllers.ts
@@ -4,6 +4,8 @@ import CustomError from "../../CustomError/CustomError.js";
 import User from "../../database/models/User.js";
 import type { RegisterData } from "../../types/types.js";
 import bcrypt from "bcrypt";
+import environment from "../../loadEnviroment.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (
   req: Request,
@@ -26,4 +28,34 @@ export const registerUser = async (
       new CustomError((error as Error).message, 500, "Something went wrong")
     );
   }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password } = req.body as RegisterData;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    next(new CustomError("Username not found", 401, "Wrong credentials"));
+    return;
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    next(new CustomError("Password is incorrect", 401, "Wrong credentials"));
+    return;
+  }
+
+  const tokenPayload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(tokenPayload, environment.jwtSecret, {
+    expiresIn: "1d",
+  });
+
+  res.status(200).json({ token });
 };
--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/usersControllers.js";
+import { loginUser, registerUser } from "../controllers/usersControllers.js";
 
 const userRouters = express.Router();
 
 userRouters.post("/register", registerUser);
+userRouters.post("/login", loginUser);
 
 export default userRouters;
