30717b483245f543599738092f982909a5625b4f
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-25 10:30:36 +0100

    Add allowedOrigins urls

---
+++ src/server/cors/allowedOrigins.ts
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localhost:4000",
+  "https://cristina-fores-final-project-202209.onrender.com/",
+  "http://localhost:3000",
+];
+
+export default allowedOrigins;
