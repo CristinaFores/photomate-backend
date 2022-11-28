d86db610d2f2a42ca88776e2fdc58a41f12b21bb
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-28 10:34:00 +0100

    Fix urls cors origins

--- src/server/cors/allowedOrigins.ts
+++ src/server/cors/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:4000",
-  " https://cristina-fores-final-project-202209.netlify.app",
-  " https://cristina-fores-final-project-202209.onrender.com",
+  "https://cristina-fores-final-project-202209.netlify.app",
+  "https://cristina-fores-final-project-202209.onrender.com",
   "http://localhost:3000",
 ];
 
