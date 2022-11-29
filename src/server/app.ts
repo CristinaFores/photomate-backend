ff0a2e92ecd000fcce9920f24ccacbcdae0e2aed
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-29 17:57:34 +0100

    Add test auth , and auth routers

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,6 +6,7 @@ import { generalError, unknownEndpoint } from "./middlewares/errors.js";
 import userRouters from "./routers/userRouters/usersRouters.js";
 import corsOptions from "./cors/corsOptions.js";
 import postsRouters from "./routers/postRouters/postRouters.js";
+import { auth } from "./middlewares/auth/auth.js";
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/users", userRouters);
-app.use("/users", postsRouters);
+app.use("/post", auth, postsRouters);
 app.use(unknownEndpoint);
 app.use(generalError);
 
