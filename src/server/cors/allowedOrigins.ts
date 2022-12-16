210f0343c386b3b16c9a684556cfd38e037c1554
Cristina <cristinaforescampos1992@gmail.com>
2022-12-16 23:56:47 +0100

    Fix allowedOrigins

--- src/server/cors/allowedOrigins.ts
+++ src/server/cors/allowedOrigins.ts
@@ -1,9 +1,8 @@
 const allowedOrigins = [
   "http://localhost:4000",
-  "https://cristina-fores-final-project-202209.onrender.com",
+  " https://https-cristina-fores-final-project.onrender.com",
   " https://photomate-cristina.netlify.app",
   "http://localhost:3000",
-  "https://https-cristina-fores-final-project.onrender.com",
 ];
 
 export default allowedOrigins;
